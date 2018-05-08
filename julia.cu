2fe3630f4854abee631b5774838f479bc3ce898b
Andrew Danner <adanner@cs.swarthmore.edu>
2018-05-08 11:53:33 -0400

    Julia set fractal demo

--- julia.cu
+++ julia.cu
@@ -7,12 +7,14 @@
 // you register animation and clean-up events with this libary
 // and then run your animation.
 //
-// This example shows how to use our class to recreate the ripple animation
-// from CUDA by Example (Section 5.2.2)
+// This example creates a complex number struct and uses it
+// to generate and optionally animate Julia Set fractals. 
+//
 // (danner, 2018)
 
 #include <unistd.h>
 #include <stdio.h>
+#include <math.h>
 #include "myOpenGLlib.h"
 #include "handle_cuda_error.h"
 
@@ -20,9 +22,10 @@
 #define DIM 1024
 #define PI 3.1415926535897932f
 
-static void animate_ripple(uchar3 *disp, void *mycudadata);
+static void animate_julia(uchar3 *disp, void *mycudadata);
 static void clean_up(void* mycudadata);
-__global__ void  ripple(uchar3 *data, int size, int ticks);
+__host__ __device__ int julia( int x, int y, float re, float im);
+__global__ void  julia_kernel(uchar3 *data, int size, float re, float im);
 
 /* The GPUDisplayData class will automatically create
    an RGB buffer of a given width and height for you on
@@ -35,7 +38,9 @@ __global__ void  ripple(uchar3 *data, int size, int ticks);
 */
 typedef struct my_cuda_data {
   int size;  // width and height of image
-  int ticks; // a time tick for updating the animation
+  float re;  // real compenent of complex seed
+  float im;  // imaginary compenent of complex seed
+  int ticks; // counter
 } my_cuda_data;
 
 
@@ -45,7 +50,9 @@ int main(int argc, char *argv[])  {
   // GPUDisplayData constructor
   my_cuda_data info;
   info.size=DIM;
-  info.ticks=0;
+  info.re= -0.800;
+  info.im=  0.156;
+  info.ticks= 0;
 
   // The call to the constructor has to come before any calls to
   // cudaMalloc or other Cuda routines
@@ -53,14 +60,14 @@ int main(int argc, char *argv[])  {
   // a struct with fields which are ptrs to cudaMalloc'ed space
   // The other reason is that adding a level of indirection
   // is the answer to every problem.
-  GPUDisplayData my_display(info.size, info.size, &info, "Ripple CUDA");
+  GPUDisplayData my_display(info.size, info.size, &info, "Fractal CUDA");
 
   // register a clean-up function on exit that will free
   // any dynamically allocated memory on the GPU or the CPU
   my_display.RegisterExitFunction(clean_up);
 
   // have the library run our CUDA animation
-  my_display.AnimateComputation(animate_ripple);
+  my_display.AnimateComputation(animate_julia);
   return 0;
 }
 
@@ -85,36 +92,79 @@ static void clean_up(void* mycudadata) {
 //          constructor
 //
 // your animate function prototype must match this one:
-static void animate_ripple(uchar3 *devPtr, void *my_data) {
+static void animate_julia(uchar3 *devPtr, void *my_data) {
 
-  int tdim = 8;
   my_cuda_data *data = (my_cuda_data *)my_data;
-  dim3 blocks(data->size/tdim, data->size/tdim);
-  dim3 threads_block(tdim, tdim);
+  dim3 blocks(data->size, data->size);
 
   /* This example just needs a time tick to generate an image
      directly in the GPU pixel buffer on the fly. No additional
      GPU pointers are needed from the my_cuda_data struct.
      devPtr is created and passed by the GPUDisplayData class */
-  ripple<<<blocks,threads_block>>>(devPtr, data->size, data->ticks);
-  data->ticks += 2;
+  float im = data->im+0.2*sin(data->ticks/20.);
+  float re = data->re+0.3*cos(data->ticks/17.);
+  julia_kernel<<<blocks,1>>>(devPtr, data->size, re, im);
+  data->ticks += 1;
+
 }
 
-__global__ void ripple( uchar3* optr, int size, int ticks ) {
+__global__ void julia_kernel( uchar3* optr, int size, float re, float im ) {
     // map from threadIdx/BlockIdx to pixel position
-    int x = threadIdx.x + blockIdx.x * blockDim.x;
-    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int x = blockIdx.x;
+    int y = blockIdx.y;
     int offset = x + y * size;
 
-    // compute distance from center of image
-    float fx = x - size/2;
-    float fy = y - size/2;
-    float d = sqrtf( fx * fx + fy * fy );
-    unsigned char grey = (unsigned char)(128.0f + 127.0f *
-                                         cos(d/10.0f - ticks/7.0f) /
-                                         (d/10.0f + 1.0f));
-    optr[offset].x = grey;
-    optr[offset].y = grey;
-    optr[offset].z = grey;
 
+    // now calculate the value at that position
+    int juliaValue = julia( x, y, re, im );
+    if (juliaValue == -1){ /* in the set */
+      optr[offset].x = 99;
+      optr[offset].y = 25;
+      optr[offset].z = 25;
+    }
+    else if (juliaValue < 10) {
+      optr[offset].x = 0;
+      optr[offset].y = 255 * juliaValue/10.;
+      optr[offset].z = 0;
+    }
+    else {
+      float t = juliaValue/200.;
+      optr[offset].x = 0;
+      optr[offset].y = 0;
+      optr[offset].z = 255*t;
+    }
+
+}
+
+struct cuComplex {
+    float   r;
+    float   i;
+    __host__ __device__ cuComplex( float a, float b ) : r(a), i(b)  {}
+    __host__ __device__ float magnitude2( void ) {
+        return r * r + i * i;
+    }
+    __host__ __device__ cuComplex operator*(const cuComplex& a) {
+        return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
+    }
+    __host__ __device__ cuComplex operator+(const cuComplex& a) {
+        return cuComplex(r+a.r, i+a.i);
+    }
+};
+
+__host__ __device__ int julia( int x, int y, float re, float im ) {
+    const float scale = 1.5;
+    float jx = scale * (float)(DIM/2 - x)/(DIM/2);
+    float jy = scale * (float)(DIM/2 - y)/(DIM/2);
+
+    cuComplex c(re, im);
+    cuComplex a(jx, jy);
+
+    int i = 0;
+    for (i=0; i<200; i++) {
+        a = a * a + c;
+        if (a.magnitude2() > 1000)
+            return i;
+    }
+
+    return -1;
 }
